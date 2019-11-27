f30f15862a03f63381b37c68b6ea927ca4fa3081
Jacob Butler <jak@Jacobs-MacBook-Pro.local>
2019-11-27 16:39:11 -0500

    I think I'm still having async issues when logging things to the console. The property error for the returned observable is fixed.

--- src/app/display-list/display-list.component.ts
+++ src/app/display-list/display-list.component.ts
@@ -27,21 +27,80 @@ export class DisplayListComponent implements OnInit {
   }
 
   loadUserSeqFiles()
-  {
-    this.firestore.collection("sequencerFiles").valueChanges()
-    .subscribe(v =>{
-      for(var i = 0;i<v.length;i++){
-        console.log('in subscribe',v[i]);
-        //console.log(v[i].fileName);
-        // console.log(v[i].owner);
-        //this.list.push(v[i]);
-        // // this.list.push(new file());
-        //this.list.push(new File(v[i].fileName, v[i].owner, v[i].pattern, v[i].sounds));
-        console.log('first list index',this.list[1]);
-        //console.log('the whole list',this.list);
+  {                  /* This block will allow me to create objects from the */
+                    /* data that is returned. */
 
+    this.firestore.collection<File>('sequencerFiles').valueChanges()
+    .subscribe(v =>{
+      console.log(v);
+      console.log(v[0]);
+      console.log(v[0].fileName);
+      for( var i = 0; i<v.length;i++)
+      {
+        this.list.push(new File(v[i].fileName, v[i].owner, v[i].pattern, v[i].sounds));
       }
-    });
+
+    })
+
+    console.log("all the sequencerFiles from the database: ",this.list);
+    for( var i = 0; i<this.list.length;i++)
+    {
+      console.log("list item ",i,": ",this.list[0]);
+    }
+
+
+                /* This block is almost identical except for the use of
+                snapshotChanges instead of value changes. I want to use This
+                because the id of the object on the database is returned
+                as part of the Observable.
+                this makes crud easier?? also multiple files with the same name?
+                But I cannot get the data i need out of the insane json tree. big sad.
+                */
+
+    // this.firestore.collection<File>('sequencerFiles').snapshotChanges()
+    // .subscribe(v =>{
+    //   console.log(v);
+    //   console.log(v[0]);
+    //   console.log(v[0].fileName);
+    //   for( var i = 0; i<v.length;i++)
+    //   {
+    //     this.list.push(new File(v[i].fileName, v[i].owner, v[i].pattern, v[i].sounds));
+    //   }
+    //
+    // })
+
+
+
+
+
+
+    // this.firestore.collection<File>("sequencerFiles").snapshotChanges().pipe(map(files =>{
+    //   const file = files[0];
+    //   console.log(files[0])
+    //   console.log(file);
+    // }))
+    // console.log('fuck angular and angulars mother');
+    // console.log(this.files);
+
+
+
+
+
+
+    // this.firestore.collection("sequencerFiles").valueChanges()
+    // .subscribe(v =>{
+    //   for(var i = 0;i<v.length;i++){
+    //     console.log('in subscribe',v[i]);
+    //     //console.log(v[i].fileName);
+    //     // console.log(v[i].owner);
+    //     //this.list.push(v[i]);
+    //     // // this.list.push(new file());
+    //     this.list.push(new File(v[i].fileName, v[i].owner, v[i].pattern, v[i].sounds));
+    //     console.log('first list index',this.list[1]);
+    //     //console.log('the whole list',this.list);
+    //
+    //   }
+    // });
   }
 
 
